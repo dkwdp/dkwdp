e6f1d024cb3bb5c754ab9822af72ac09022cdd0e
duncanimi <duncantomke1@gmail.com>
2025-11-16 21:03:01 +0200

    Added Textelement to adapt to videoelement and level branch

--- src/lib/level_elements/TextElement.svelte
+++ src/lib/level_elements/TextElement.svelte
@@ -3,6 +3,19 @@
 	export let element: TextElementData;
 </script>
 
-<div>
+<div class= "text-container">
 	{element.markdown}
 </div>
+
+<style>
+	.text-container {
+	max-width: 950px;
+	margin: 0 auto;
+	padding: 1rem;
+	line-height: 1.6;
+	font-family: Arial, sans-serif;
+	color: white; 
+
+	}
+
+</style>
