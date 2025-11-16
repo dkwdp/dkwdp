69cff4954c4e70324e75ef4c2224f2e03d9573e9
duncanimi <duncantomke1@gmail.com>
2025-11-16 20:07:33 +0200

    Videoelement added centered and responsive

--- src/lib/level_elements/VideoElement.svelte
+++ src/lib/level_elements/VideoElement.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
 	import type { VideoElementData } from '$lib/types';
-
 	export let element: VideoElementData;
+
+	function embedURL(url: string): string {
+		if (url.includes('youtube.com') || url.includes('youtu.be')) {
+			const videoId = url.split('v=')[1]?.split('&')[0] || url.split('/').pop()?.split('?')[0];
+			return `https://www.youtube.com/embed/${videoId}`;
+		}
+		return url;
+	}
 </script>
 
-<!--
-<video class="w-full rounded" controls src={data.level.url}><track src="" kind="captions" /></video>
-{#if data.level.transcript}
-  <details><summary>Transcript</summary><pre>{data.level.transcript}</pre></details>
-{/if}
-<iframe width="1298" height="730" src="{data.video_url}" title="{data.title}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
--->
-<iframe width="1024" height="576" src="{element.url}" title="Some Video" allow="web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<div class="video-container">
+<iframe src="{embedURL(element.url)}" title="Some Video" allow="web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+<style>
+	.video-container {
+		display : flex; 
+		justify-content: center;
+		width: 100%;
+		max-width: 950px;
+		margin: 0 auto;
+		
+	}
+	.video-container iframe {
+		max-width: 100%;
+		aspect-ratio: 16 / 9;
+		height: auto;
+	}
+</style>
