f003adf25cb91e3a69a068350a47a0476526ce03
Duncan <duncantomke2@gmail.com>
2025-12-02 21:40:39 +0200

    Update src/routes/map/[mapId]/level/[levelId]/+page.ts
    Co-authored-by: Luis Hankel <kaes3kuch3n@gmail.com>

--- src/routes/map/[mapId]/level/[levelId]/+page.ts
+++ src/routes/map/[mapId]/level/[levelId]/+page.ts
@@ -1,9 +1,9 @@
 import { Level as LevelSchema } from '$lib/schemas';
 
 export const load = async ({ params, fetch }) => {
-  const res = await fetch(`/content/${params.mapId}/levels/${params.levelId}/content.json`);
-  if (!res.ok) throw new Error('level content not found');
-  const json = await res.json();
+  const response = await fetch(`/content/${params.mapId}/levels/${params.levelId}/content.json`);
+  if (!response.ok) throw new Error('level content not found');
+  const level = await response.json();
   
   // Load external markdown files
   for (const element of json.elements) {
