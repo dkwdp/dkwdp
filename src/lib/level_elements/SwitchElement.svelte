77c8384ca7bf834722d4c767d03adaa8bd33162f
jonathan-d-z <jonathan.d.zieger@gmail.com>
2025-12-12 18:40:52 +0100

    files formatted for pull request

--- src/lib/level_elements/SwitchElement.svelte
+++ src/lib/level_elements/SwitchElement.svelte
@@ -6,36 +6,57 @@
 	export let element: SwitchElementData;
 	const countChoices = element.next_levels.length;
 
-  function go(chosen: { levelId: any }) {
-    normalize_and_goto(chosen.levelId, page.params.mapId);
-  }
+	function go(chosen: { levelId: any }) {
+		normalize_and_goto(chosen.levelId, page.params.mapId);
+	}
 </script>
+
+<!--<button class="px-3 py-2 rounded-lg shadow" on:click={go}>
+	Nice description of level {normalize_level_id(element.next_level, page.params.mapId)}.
+</button>-->
+<div class="imageRow" class:two={countChoices == 2} class:three={countChoices == 3}>
+	{#each element.next_levels as choice}
+		<button class="imageButton" on:click={() => go(choice)}>
+			<img
+				class="image"
+				src={'/content/' +
+					page.params.mapId +
+					'/levels/' +
+					page.params.levelId +
+					'/' +
+					choice.image}
+				alt=""
+			/>
+		</button>
+	{/each}
+</div>
+
 <style>
-	.imageButton{
-		padding: 0; 
-		background: none; 
-		display: inline-block; 
-		border-radius: 12px; 
-		border: 3px solid #0b0b0b; 
+	.imageButton {
+		padding: 0;
+		background: none;
+		display: inline-block;
+		border-radius: 12px;
+		border: 3px solid #0b0b0b;
 		box-shadow: 0px 6px #0b0b0b;
 
-        transition:
-            transform 0.1s ease-out,
-            box-shadow 0.1s ease-out;
-        cursor: pointer;
+		transition:
+			transform 0.1s ease-out,
+			box-shadow 0.1s ease-out;
+		cursor: pointer;
 	}
 
 	.imageButton:hover {
-        transform: translateY(3px);
-        box-shadow: 0px 3px #0b0b0b;
-    }
+		transform: translateY(3px);
+		box-shadow: 0px 3px #0b0b0b;
+	}
 
-	.two .imageButton{
+	.two .imageButton {
 		width: 33%;
 		height: auto;
 	}
 
-	.three .imageButton{
+	.three .imageButton {
 		width: 24%;
 		height: auto;
 	}
@@ -53,13 +74,3 @@
 		display: block;
 	}
 </style>
-<!--<button class="px-3 py-2 rounded-lg shadow" on:click={go}>
-	Nice description of level {normalize_level_id(element.next_level, page.params.mapId)}.
-</button>-->
-<div class="imageRow" class:two={countChoices == 2} class:three={countChoices == 3}>
-{#each element.next_levels as choice}
-  <button class="imageButton" on:click={() => go(choice)}>
-    <img class="image" src={"/content/" + page.params.mapId + "/levels/" + page.params.levelId + "/" + choice.image} alt="" />
-  </button>
-{/each}
-</div>
